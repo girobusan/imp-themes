85ca9e24a4e0174daf5072bfb92b1532d7c91601
Gleb TZ <gleb@>
2024-03-14 22:36:00 +0300

    theme updates

--- automation/build.sh
+++ automation/build.sh
@@ -1,10 +1,10 @@
 npx sass  \
-  src/styleswitch.scss:dist/styleswitch.css\
-  src/netart.scss:dist/netart.css\
-  src/tbimp_bw.scss:dist/base_bw.css\
-  src/tbimp_bw_inverted.scss:dist/base_bw_i.css\
-  src/default.scss:dist/style.css\
-  src/simplecss.scss:dist/simplecss.css
+  src/styleswitch.scss:docs/styleswitch.css\
+  src/netart.scss:docs/netart.css\
+  src/tbimp_bw.scss:docs/base_bw.css\
+  src/tbimp_bw_inverted.scss:docs/base_bw_i.css\
+  src/default.scss:docs/style.css\
+  src/simplecss.scss:docs/simplecss.css
 
 cp test/sampler.html docs/
 cp test/styleswitch.css docs/
